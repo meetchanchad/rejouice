08da895eaab508ecc6c9e756d5ba0721a316a226
Meet Chanchad <123048686+meetchanchad@users.noreply.github.com>
2023-12-09 15:18:39 +0000

    Loader

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function playReelMover(){
     let pageOneContent = document.querySelector(".pageOne-content")
-let cursor = document.querySelector("#cursor")
+    let cursor = document.querySelector("#cursor")
 
 pageOneContent.addEventListener("mousemove" ,function(dets){
     gsap.to("#cursor",{
@@ -23,3 +23,31 @@ pageOneContent.addEventListener("mouseleave", function(){
 })
 }
 playReelMover()
+
+let tl = gsap.timeline()
+
+tl.from("#loader h3",{
+    x:40,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+tl.to("#loader h3",{
+    opacity:0,
+    x:-40,
+    duration:0.5,
+    delay:0.5,
+    stagger:0.1
+})
+
+tl.to("#loader",{
+    opacity:0,
+    display: "none"
+})
+tl.from(".pageOne-content h1 span",{
+    y:150,
+    opacity:0,
+    stagger:0.1,
+    duration:0.4,
+    delay:-0.5
+})
